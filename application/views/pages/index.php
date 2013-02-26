475b8a56e012b8a10134e03cac2536036a16dea8
Brian Kracoff <info@nahyou.com>
2013-02-25 20:38:36 -0500

    more css

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -35,18 +35,28 @@
                     <td class="error"><?php echo form_error('last_name'); ?></td>
                 </tr>
 
-                <tr><td><label for="email">Email<span class="required">*</span></label></td></tr>
-                <tr><td><input type="text" name="email" value="<?php echo set_value('email'); ?>"/></td></tr>
-                
-                <tr><td><label for="reenter_email">Email again<span class="required">*</span></label></td></tr>
-                <tr><td><input type="text" name="reenter_email" /></td></tr>
+                <tr>
+                    <td><label for="email">Email<span class="required">*</span></label></td>
+                    <td><label for="reenter_email">Email again<span class="required">*</span></label></td>
+                </tr>
+
+                <tr>
+                    <td><input type="text" name="email" value="<?php echo set_value('email'); ?>"/></td>
+                    <td><input type="text" name="reenter_email" /></td>
+                </tr>
+
                 <tr><td class="error"><?php echo form_error('email'); ?></td></tr>
                 
-                <tr><td><label for="password">Password<span class="required">*</span></label></td></tr>
-                <tr><td><input type="password" name="password" /></td></tr>
-                
-                <tr><td><label for="reenter_password">Password again<span class="required">*</span></label></td></tr>
-                <tr><td><input type="password" name="reenter_password" /></td></tr>
+                <tr>
+                    <td><label for="password">Password<span class="required">*</span></label></td>
+                    <td><label for="reenter_password">Password again<span class="required">*</span></label></td>
+                </tr>
+
+                <tr>
+                    <td><input type="password" name="password" /></td>
+                    <td><input type="password" name="reenter_password" /></td>
+                </tr>
+
                 <tr><td class="error"><?php echo form_error('password'); ?></td></tr>
 
                 <tr><td><input type="submit" value="Sign up" /></td></tr>
