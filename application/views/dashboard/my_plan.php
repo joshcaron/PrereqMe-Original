ca0a363eacf5b352fa32f743c8fdbf95c4cfaf04
Brian Kracoff <info@nahyou.com>
2013-02-26 18:08:24 -0500

    my plan

--- application/views/dashboard/my_plan.php
+++ application/views/dashboard/my_plan.php
@@ -5,14 +5,14 @@
             <div id="sortable_<?php echo $semester->id?>"  class="semester box">
                 <div class="semester_title"><?php echo $semester->title?></div>
                 <?php 
-                /*for($courseIndex = 0; $courseIndex < count($semester->courses); $courseIndex++)
+                for($courseIndex = 0; $courseIndex < count($semester->courses); $courseIndex++)
                 {
                     $course = $semester->courses[$courseIndex];
                     echo '<div class="course">';
                     echo $course->deptCode, $course->code, ' - ';
                     echo $course->title, ' (', $course->credits, ')'; 
                     echo '</div>';
-                }*/
+                }
                 ?>
                 <ul id="semester_1" class="connectedSortable">
                     <li class="ui-state-default">Item 1</li>
