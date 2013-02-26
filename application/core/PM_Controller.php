ce9f49185575ec1350da7dc432daa440cbb2032a
Brian Kracoff <info@nahyou.com>
2013-02-26 11:29:15 -0500

    sign-up workflow

--- application/core/PM_Controller.php
+++ application/core/PM_Controller.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class PM_Base extends CI_Controller
+class PM_Controller extends CI_Controller
 {
     public function __construct()
     {
