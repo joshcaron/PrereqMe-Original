a2f321d1e9313cd0ffd7daaa437f6dbb850eae09
Brian Kracoff <info@nahyou.com>
2013-02-25 22:37:30 -0500

    search bar styles

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -12,7 +12,9 @@
 
             <div id="query_box">
                 <div class="magnifying_large"></div>
-                <input id="query" class="fl" type="text" name="query" class="search" placeholder="Find course by id or title..." disabled/>
+                <div>
+                    <input id="query" class="fl" type="text" name="query" class="search" placeholder="Find course by id or title..." disabled/>
+                </div>
             </div>
             <div>
                 <input type="submit" value="Search"/>
