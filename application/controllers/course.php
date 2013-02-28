d9285e3f67f1732df00a562b99db409ab36a021d
Brian Kracoff <info@nahyou.com>
2013-02-27 19:25:23 -0500

    search autocomplete

--- application/controllers/course.php
+++ application/controllers/course.php
@@ -93,7 +93,7 @@ class Course extends PM_Controller
 
             foreach($fullCourses as $course)
             {
-                $courseTitles[] = $course->title;
+                $courseTitles[] = $course->deptCode.$course->code.' - '.$course->title;
             }
 
             $data['response'] = $courseTitles;
