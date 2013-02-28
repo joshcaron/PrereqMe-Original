93dd038e8345be16485c736094179c7c3b8ccf8f
Brian Kracoff <info@nahyou.com>
2013-02-27 20:19:18 -0500

    my plan

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -130,33 +130,6 @@ class Home extends PM_Controller
             $this->load->view('json', $data);
         }
     }
-
-    //Performs a search and prints an array of titles
-    //Used by an AJAX call for the search typeahead
-    public function search()
-    {
-        $collegeId = $this->input->get('collegeId');
-        $query = $this->input->get('term'); //use 'term' instead of 'query' here because it is automatically set by jQueryUI
-
-        if($collegeId === FALSE || $query === FALSE)
-        {
-            log_message('error', 'GET Params were not received by search.index');
-        }
-        else
-        {
-            $fullCourses = $this->course_model->get_like_title($collegeId, $query);
-
-            $courseTitles = array();
-
-            foreach($fullCourses as $course)
-            {
-                $courseTitles[] = $course->title;
-            }
-
-            $data['response'] = $courseTitles;
-            $this->load->view('json', $data);
-        }
-    }
 }
 
 
