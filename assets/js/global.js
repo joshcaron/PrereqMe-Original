0f48057051e281f43116e300f6bd403757c2d775
Brian Kracoff <info@nahyou.com>
2013-03-01 09:23:27 -0500

    deleting a semester

--- assets/js/global.js
+++ assets/js/global.js
@@ -174,8 +174,8 @@ var myplan = {
         var deleteUrl = "delete_semester_from_user?&semesterId=" + myplan.semesterIdFromSemesterUL(parentUl);
         $.getJSON(deleteUrl ,null);
 
-        //Hide list element
-        parentUl.hide('blind', {}, 250, null);       
+        //Hide box element
+        parentUl.parent()hide('blind', {}, 250, null);       
     },
 
     //Gets the semester Id from the semester UL element
