89509a1459df421bd78d3e8345d82eb2483bfa21
Brian Kracoff <info@nahyou.com>
2013-03-04 18:40:47 -0500

    browse functionality

--- assets/js/browse.js
+++ assets/js/browse.js
@@ -20,8 +20,6 @@ var browse = {
         {
             var getUrl = BASE_URL + "index.php/dashboard/courses_for_filters?deptId=" + deptId;
 
-            var dataTable = $('#BROWSE #RESULTS').first().dataTable();
-
             //Get filtered courses from server
             $.getJSON(getUrl ,function(result){
                     var tableBody = $('#BROWSE #RESULTS tbody').first();
@@ -39,9 +37,6 @@ var browse = {
                         tableRow += "</tr>";
                         tableBody.append(tableRow);
                     });
-
-                    //Redraw Table
-                    dataTable.fnDraw();
             });
         }
     }
