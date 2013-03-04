d74a22bce4f06ad1cff25a8f121e4d70d36d3c4d
Brian Kracoff <info@nahyou.com>
2013-03-04 16:43:21 -0500

    browse functionality

--- application/views/dashboard/browse.php
+++ application/views/dashboard/browse.php
@@ -1,5 +1,30 @@
 <div id="BROWSE">
 
-<h1>BROWSE!</h1>
+<div id="FILTERS">
+
+    <select id="dept_id" name="deptId">
+        <option value="0" selected>Select department</option>
+
+        <?php foreach($departments as $department): ?>
+            <option value=<?php echo $department->id?> ><?php echo $department->code?></option>
+        <?php endforeach ?>
+    </select>
+</div>
+
+<table id="RESULTS">
+    <thead>
+        <tr>
+            <th>Dept. Code</th>
+            <th>Course Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Credits</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
+<script>browse.initializeTable();</script>
 
 </div>
--- assets/js/browse.js
+++ assets/js/browse.js
@@ -1,6 +1,14 @@
 //Functions for the browse page
 var browse = {
 
+    initializeTable : function()
+    {
+        $('#BROWSE #RESULTS').first().dataTable();
+    },
 
+    //Updates the courses based on the set filters
+    updateCourses : function()
+    {
 
+    }
 };
