9315704ffc590e357d7a3a5adfaf3a5755aac428
Brian Kracoff <info@nahyou.com>
2013-03-07 13:59:14 -0500

    prereq graph on detail page

--- assets/js/spacetree/course.js
+++ assets/js/spacetree/course.js
@@ -78,11 +78,7 @@ function initWithJSON(json)
             label.id = node.id;            
             label.innerHTML = node.name;
             label.onclick = function(){
-            	if(normal && normal.checked) {
             	  st.onClick(node.id);
-            	} else {
-                st.setRoot(node.id, 'animate');
-            	}
             };
             //set label styles
             var style = label.style;
