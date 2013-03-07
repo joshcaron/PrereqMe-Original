2baab412a27258a77b23ca0918835802ff53d09b
Brian Kracoff <info@nahyou.com>
2013-03-07 13:51:40 -0500

    prereq graph on detail page

--- assets/js/spacetree/course.js
+++ assets/js/spacetree/course.js
@@ -138,7 +138,7 @@ function initWithJSON(json)
     //compute node positions and layout
     st.compute();
     //optional: make a translation of the tree
-    st.geom.translate(new $jit.Complex(-400, 0), "current");
+    st.geom.translate(new $jit.Complex(-800, 0), "current");
     //emulate a click on the root node.
     st.onClick(st.root);
     //end
