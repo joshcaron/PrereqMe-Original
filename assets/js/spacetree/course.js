b5491345b914dcb066d905cda89478fe40f88553
Brian Kracoff <info@nahyou.com>
2013-03-07 20:11:12 -0500

    prereq graph on detail page

--- assets/js/spacetree/course.js
+++ assets/js/spacetree/course.js
@@ -51,7 +51,6 @@ function initWithJSON(json)
         //set overridable=true for styling individual
         //nodes or edges
         Node: {
-            autoHeight: true,
             autoWidth: true,
             type: 'rectangle',
             color: '#aaa',
