9121dc2e00ddc8e39770bf3ad0eda62bacf2c846
Brian Kracoff <info@nahyou.com>
2013-03-08 10:49:38 -0500

    prereq graph on detail page

--- assets/js/spacetree/course.js
+++ assets/js/spacetree/course.js
@@ -44,7 +44,7 @@ function initWithJSON(json)
         //set orientation
         orientation: 'right',
         //set levels to show
-        levelsToShow: 6,
+        levelsToShow: 15,
         constrained: false,
         siblingOffset: 50,
 
