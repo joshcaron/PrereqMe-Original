a1f5e122e996f55143519741ae9593d7d2ce18f5
Brian Kracoff <info@nahyou.com>
2013-03-08 09:43:00 -0500

    prereq graph on detail page

--- application/views/course/detail.php
+++ application/views/course/detail.php
@@ -1,10 +1,8 @@
 <div id="COURSE_DETAIL">
 
-<h2><?php echo $course->title, " - ", $course->deptCode, $course->code ?></h2>
+<h2><?php echo $course->deptCode, $course->code, $course->title, " - " ?></h2>
 <p><?php echo $course->credits ?> credits</p>
 <p><?php echo $course->description ?></p>
-
-<h2>Prereqs</h2>
 
 <div id="infovis"></div>
 
