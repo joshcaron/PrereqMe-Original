9a8ce4c203f657f6599935d582d86209bac87042
Brian Kracoff <bkracoff@gmail.com>
2013-03-11 18:20:41 -0400

    change text color to white

--- application/views/course/search_results.php
+++ application/views/course/search_results.php
@@ -2,13 +2,12 @@
 
 <div id="content">
 
-<div class="results_text">Found <?php echo count($results)?> results for "<?php echo $query?>":</div>
+<div id="results_text">Found <?php echo count($results)?> results for "<?php echo $query?>":</div>
 <?php foreach($results as $course)
 {
     $url = base_url('index.php/course/view/' . $course->id);
     echo '<div class="course" onclick="window.location.href =\'', $url, '\';">';
-    echo $course->deptCode, $course->code, ' - ';
-    echo $course->title, ' (', $course->credits, ')';
+    echo $course->deptCode, $course->code, ' - ', $course->title;
     echo '</div>';
 }
 ?>
