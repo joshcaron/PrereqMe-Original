bcd8e988c8080cf32be9a93dae57f9abbd437903
Brian Kracoff <bkracoff@gmail.com>
2013-03-13 09:52:52 -0400

    add degree audit

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,7 +1,101 @@
 <div ID="OVERVIEW">
 
-<h1>Overview of stuff!</h1>
+<h1>You've completed ___% of your requirements!</h1>
 
-<p>Will display a degree audit that will show users what courses they need to complete to finish their major requirements.</p>
+<h3>Note, the following data is static (for now)</h3>
+
+<div id="DEGREE_AUDIT">
+    <h3>Computer Science Major Requirements</h3>
+    <div>
+        <ul>
+            <li>CS 1200 - Computer Science/Information Science Overview 1</li>
+            <li>CS 1210 - Computer Science/Information Science Overview 2: Co-op Preparation</li>
+            <li>CS 2500 - Fundamentals of Computer Science 1</li>
+            <li>CS 2501 - Lab for CS 2500</li>
+            <li>CS 2510 - Fundamentals of Computer Science 2</li>
+            <li>CS 2511 - Lab for CS 2510</li>
+            <li>CS 2800 - Logic and Computation</li>
+            <li>CS 2801 - Lab for CS 2800</li>
+            <li>CS 2600 - Computer Organization</li>
+            <li>CS 3800 - Theory of Computation</li>
+            <li>CS 3600 - Systems and Networks</li>
+            <li>CS 4400 - Programming Languages</li>
+            <li>CS 4500 - Software Development</li>
+            <li>CS 4800 - Algorithms and Data</li>
+            <li>CS 4000 - Senior Seminar</li>
+            <li>Computer Science Capstone</li>
+            <li>Computer Science Upper Division Elective 1</li>
+            <li>Computer Science Upper Division Elective 2</li>
+        </ul>
+    </div>
+
+    <h3>CS Additional Courses for BS</h3>
+    <div>
+        <ul>
+            <li>MATH 2331 - Linear Algebra</li>
+            <li>MATH 3081 - Probability and Statistics</li>
+            <li>SOCL 4528 - Computers and Society</li>
+            <li>Science Requirement 1</li>
+            <li>Science Requirement 2</li>
+            <li>Science Requirement 3</li>
+            <li>Science Requirement 4</li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+
+    <h3>Computer Science English Requirement</h3>
+    <div>
+        <ul>
+            <li>ENGL 1111 - College Writing</li>
+            <li>ENGL 3301 - Advanced Writing in the Disciplines</li>
+            <li>ENGL 3302 - Advanced Writing in the Technical Professions</li>
+        </ul>
+    </div>
+
+    <h3>General Electives</h3>
+    <div>
+        <ul>
+            <li>General Elective 1</li>
+            <li>General Elective 2</li>
+            <li>General Elective 3</li>
+            <li>General Elective 4</li>
+            <li>General Elective 5</li>
+            <li>General Elective 6</li>
+            <li>General Elective 7</li>
+            <li>General Elective 8</li>
+        </ul>    
+    </div>
+
+    <h3>NU Core</h3>
+    <div>
+        <ul>
+            <li>College Writing</li>
+            <li>Arts and Humanities Level 1</li>
+            <li>Social Science Level 1</li>
+            <li>Science/Technology Level 1</li>
+            <li>Comparative Study of Cultures</li>
+            <li>Analytical Math Level 1</li>
+            <li>Writing Intensive</li>
+            <li>Level 2 Elective</li>
+            <li>Capstone / Writing Intensive II</li>
+            <li>Experiential Learning</li>
+            <li>Cooperative Education</li>
+            <li>General Electives</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>    
+    </div>
+</div>
+
+<script>
+$(function() {
+    $( "#DEGREE_AUDIT" ).accordion({
+      collapsible: true
+    });
+  });
+</script>
 
 </div>
