0e6cf428a8917b785d244a2fd9915bf2fe67e2de
Brian Kracoff <bkracoff@gmail.com>
2013-03-13 10:05:01 -0400

    add degree audit

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -8,78 +8,78 @@
     <h3>Computer Science Major Requirements</h3>
     <div>
         <ul>
-            <li>CS 1200 - Computer Science/Information Science Overview 1</li>
-            <li>CS 1210 - Computer Science/Information Science Overview 2: Co-op Preparation</li>
-            <li>CS 2500 - Fundamentals of Computer Science 1</li>
-            <li>CS 2501 - Lab for CS 2500</li>
-            <li>CS 2510 - Fundamentals of Computer Science 2</li>
-            <li>CS 2511 - Lab for CS 2510</li>
-            <li>CS 2800 - Logic and Computation</li>
-            <li>CS 2801 - Lab for CS 2800</li>
-            <li>CS 2600 - Computer Organization</li>
-            <li>CS 3800 - Theory of Computation</li>
-            <li>CS 3600 - Systems and Networks</li>
-            <li>CS 4400 - Programming Languages</li>
-            <li>CS 4500 - Software Development</li>
-            <li>CS 4800 - Algorithms and Data</li>
-            <li>CS 4000 - Senior Seminar</li>
-            <li>Computer Science Capstone</li>
-            <li>Computer Science Upper Division Elective 1</li>
-            <li>Computer Science Upper Division Elective 2</li>
+            <li class="ui-state-default">CS 1200 - Computer Science/Information Science Overview 1</li>
+            <li class="ui-state-default">CS 1210 - Computer Science/Information Science Overview 2: Co-op Preparation</li>
+            <li class="ui-state-default">CS 2500 - Fundamentals of Computer Science 1</li>
+            <li class="ui-state-default">CS 2501 - Lab for CS 2500</li>
+            <li class="ui-state-default">CS 2510 - Fundamentals of Computer Science 2</li>
+            <li class="ui-state-default">CS 2511 - Lab for CS 2510</li>
+            <li class="ui-state-default">CS 2800 - Logic and Computation</li>
+            <li class="ui-state-default">CS 2801 - Lab for CS 2800</li>
+            <li class="ui-state-default">CS 2600 - Computer Organization</li>
+            <li class="ui-state-default">CS 3800 - Theory of Computation</li>
+            <li class="ui-state-default">CS 3600 - Systems and Networks</li>
+            <li class="ui-state-default">CS 4400 - Programming Languages</li>
+            <li class="ui-state-default">CS 4500 - Software Development</li>
+            <li class="ui-state-default">CS 4800 - Algorithms and Data</li>
+            <li class="ui-state-default">CS 4000 - Senior Seminar</li>
+            <li class="ui-state-default">Computer Science Capstone</li>
+            <li class="ui-state-default">Computer Science Upper Division Elective 1</li>
+            <li class="ui-state-default">Computer Science Upper Division Elective 2</li>
         </ul>
     </div>
 
     <h3>CS Additional Courses for BS</h3>
     <div>
         <ul>
-            <li>MATH 2331 - Linear Algebra</li>
-            <li>MATH 3081 - Probability and Statistics</li>
-            <li>SOCL 4528 - Computers and Society</li>
-            <li>Science Requirement 1</li>
-            <li>Science Requirement 2</li>
-            <li>Science Requirement 3</li>
-            <li>Science Requirement 4</li>
+            <li class="ui-state-default">MATH 2331 - Linear Algebra</li>
+            <li class="ui-state-default">MATH 3081 - Probability and Statistics</li>
+            <li class="ui-state-default">SOCL 4528 - Computers and Society</li>
+            <li class="ui-state-default">Science Requirement 1</li>
+            <li class="ui-state-default">Science Requirement 2</li>
+            <li class="ui-state-default">Science Requirement 3</li>
+            <li class="ui-state-default">Science Requirement 4</li>
         </ul>
     </div>
 
     <h3>Computer Science English Requirement</h3>
     <div>
         <ul>
-            <li>ENGL 1111 - College Writing</li>
-            <li>ENGL 3301 - Advanced Writing in the Disciplines</li>
-            <li>ENGL 3302 - Advanced Writing in the Technical Professions</li>
+            <li class="ui-state-default">ENGL 1111 - College Writing</li>
+            <li class="ui-state-default">ENGL 3301 - Advanced Writing in the Disciplines</li>
+            <li class="ui-state-default">ENGL 3302 - Advanced Writing in the Technical Professions</li>
         </ul>
     </div>
 
     <h3>General Electives</h3>
     <div>
         <ul>
-            <li>General Elective 1</li>
-            <li>General Elective 2</li>
-            <li>General Elective 3</li>
-            <li>General Elective 4</li>
-            <li>General Elective 5</li>
-            <li>General Elective 6</li>
-            <li>General Elective 7</li>
-            <li>General Elective 8</li>
+            <li class="ui-state-default">General Elective 1</li>
+            <li class="ui-state-default">General Elective 2</li>
+            <li class="ui-state-default">General Elective 3</li>
+            <li class="ui-state-default">General Elective 4</li>
+            <li class="ui-state-default">General Elective 5</li>
+            <li class="ui-state-default">General Elective 6</li>
+            <li class="ui-state-default">General Elective 7</li>
+            <li class="ui-state-default">General Elective 8</li>
         </ul>    
     </div>
 
     <h3>NU Core</h3>
     <div>
         <ul>
-            <li>College Writing</li>
-            <li>Arts and Humanities Level 1</li>
-            <li>Social Science Level 1</li>
-            <li>Science/Technology Level 1</li>
-            <li>Comparative Study of Cultures</li>
-            <li>Analytical Math Level 1</li>
-            <li>Writing Intensive</li>
-            <li>Level 2 Elective</li>
-            <li>Capstone / Writing Intensive II</li>
-            <li>Experiential Learning</li>
-            <li>Cooperative Education</li>
-            <li>General Electives</li>
+            <li class="ui-state-default">College Writing</li>
+            <li class="ui-state-default">Arts and Humanities Level 1</li>
+            <li class="ui-state-default">Social Science Level 1</li>
+            <li class="ui-state-default">Science/Technology Level 1</li>
+            <li class="ui-state-default">Comparative Study of Cultures</li>
+            <li class="ui-state-default">Analytical Math Level 1</li>
+            <li class="ui-state-default">Writing Intensive</li>
+            <li class="ui-state-default">Level 2 Elective</li>
+            <li class="ui-state-default">Capstone / Writing Intensive II</li>
+            <li class="ui-state-default">Experiential Learning</li>
+            <li class="ui-state-default">Cooperative Education</li>
+            <li class="ui-state-default">General Electives</li>
         </ul>    
     </div>
 </div>
