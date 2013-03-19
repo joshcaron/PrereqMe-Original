5f5681a3a2f4f7bb24bc5fa56ede4238f833c208
Brian Kracoff <bkracoff@gmail.com>
2013-03-19 12:03:20 -0400

    link courses in audit that can be linked

--- application/views/dashboard/audit.php
+++ application/views/dashboard/audit.php
@@ -10,81 +10,81 @@
 </div>
 
 <div id="DEGREE_AUDIT">
-    <h3>Computer Science Major Requirements</h3>
+    <h3>Computer Science Major Requirements (5/18 remaining)</h3>
     <div>
         <ul class="course_list">
-            <li class="ui-state-default">CS 1200 - Computer Science/Information Science Overview 1</li>
-            <li class="ui-state-default">CS 1210 - Computer Science/Information Science Overview 2: Co-op Preparation</li>
-            <li class="ui-state-default">CS 2500 - Fundamentals of Computer Science 1</li>
-            <li class="ui-state-default">CS 2501 - Lab for CS 2500</li>
-            <li class="ui-state-default">CS 2510 - Fundamentals of Computer Science 2</li>
-            <li class="ui-state-default">CS 2511 - Lab for CS 2510</li>
-            <li class="ui-state-default">CS 2800 - Logic and Computation</li>
-            <li class="ui-state-default">CS 2801 - Lab for CS 2800</li>
-            <li class="ui-state-default">CS 2600 - Computer Organization</li>
-            <li class="ui-state-default">CS 3800 - Theory of Computation</li>
-            <li class="ui-state-default">CS 3600 - Systems and Networks</li>
-            <li class="ui-state-default">CS 4400 - Programming Languages</li>
-            <li class="ui-state-highlight">CS 4500 - Software Development</li>
-            <li class="ui-state-default">CS 4800 - Algorithms and Data</li>
-            <li class="ui-state-highlight">CS 4000 - Senior Seminar</li>
-            <li class="ui-state-highlight">Computer Science Capstone</li>
-            <li class="ui-state-highlight">Computer Science Upper Division Elective 1</li>
-            <li class="ui-state-highlight">Computer Science Upper Division Elective 2</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 1200 - Computer Science/Information Science Overview 1<input type="hidden" value="2"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 1210 - Computer Science/Information Science Overview 2: Co-op Preparation<input type="hidden" value="3"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2500 - Fundamentals of Computer Science 1<input type="hidden" value="9"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2501 - Lab for CS 2500<input type="hidden" value="10"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2510 - Fundamentals of Computer Science 2<input type="hidden" value="11"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2511 - Lab for CS 2510<input type="hidden" value="12"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2800 - Logic and Computation<input type="hidden" value="14"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2801 - Lab for CS 2800<input type="hidden" value="15"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 2600 - Computer Organization<input type="hidden" value="13"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 3800 - Theory of Computation<input type="hidden" value="24"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 3600 - Systems and Networks<input type="hidden" value="23"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 4400 - Programming Languages<input type="hidden" value="30"/></li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">CS 4500 - Software Development<input type="hidden" value="32"/></li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">CS 4800 - Algorithms and Data<input type="hidden" value="44"/></li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">CS 4000 - Senior Seminar<input type="hidden" value="25"/></li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">Computer Science Capstone</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">Computer Science Upper Division Elective 1</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">Computer Science Upper Division Elective 2</li>
         </ul>
     </div>
 
-    <h3>CS Additional Courses for BS</h3>
+    <h3>CS Additional Courses for BS (1/7 remaining)</h3>
     <div>
         <ul class="course_list">
-            <li class="ui-state-default">MATH 2331 - Linear Algebra</li>
-            <li class="ui-state-default">MATH 3081 - Probability and Statistics</li>
-            <li class="ui-state-default">SOCL 4528 - Computers and Society</li>
-            <li class="ui-state-default">Science Requirement 1</li>
-            <li class="ui-state-default">Science Requirement 2</li>
-            <li class="ui-state-default">Science Requirement 3</li>
-            <li class="ui-state-highlight">Science Requirement 4</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">MATH 2331 - Linear Algebra</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">MATH 3081 - Probability and Statistics</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">SOCL 4528 - Computers and Society</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Science Requirement 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Science Requirement 2</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Science Requirement 3</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">Science Requirement 4</li>
         </ul>
     </div>
 
-    <h3>Computer Science English Requirement</h3>
+    <h3>Computer Science English Requirement (0/3 remaining)</h3>
     <div>
         <ul class="course_list">
-            <li class="ui-state-default">ENGL 1111 - College Writing</li>
-            <li class="ui-state-default">ENGL 3301 - Advanced Writing in the Disciplines</li>
-            <li class="ui-state-default">ENGL 3302 - Advanced Writing in the Technical Professions</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">ENGL 1111 - College Writing</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">ENGL 3301 - Advanced Writing in the Disciplines</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">ENGL 3302 - Advanced Writing in the Technical Professions</li>
         </ul>
     </div>
 
-    <h3>General Electives</h3>
+    <h3>General Electives (2/8 remaining)</h3>
     <div>
         <ul class="course_list">
-            <li class="ui-state-default">General Elective 1</li>
-            <li class="ui-state-default">General Elective 2</li>
-            <li class="ui-state-default">General Elective 3</li>
-            <li class="ui-state-default">General Elective 4</li>
-            <li class="ui-state-default">General Elective 5</li>
-            <li class="ui-state-default">General Elective 6</li>
-            <li class="ui-state-highlight">General Elective 7</li>
-            <li class="ui-state-highlight">General Elective 8</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 2</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 3</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 4</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 5</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">General Elective 6</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">General Elective 7</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">General Elective 8</li>
         </ul>    
     </div>
 
-    <h3>NU Core</h3>
+    <h3>NU Core (1/12 remaining)</h3>
     <div>
         <ul class="course_list">
-            <li class="ui-state-default">College Writing</li>
-            <li class="ui-state-default">Arts and Humanities Level 1</li>
-            <li class="ui-state-default">Social Science Level 1</li>
-            <li class="ui-state-default">Science/Technology Level 1</li>
-            <li class="ui-state-default">Comparative Study of Cultures</li>
-            <li class="ui-state-default">Analytical Math Level 1</li>
-            <li class="ui-state-default">Writing Intensive</li>
-            <li class="ui-state-default">Level 2 Elective</li>
-            <li class="ui-state-default">Capstone / Writing Intensive II</li>
-            <li class="ui-state-default">Experiential Learning</li>
-            <li class="ui-state-default">Cooperative Education</li>
-            <li class="ui-state-highlight">General Electives</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">College Writing</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Arts and Humanities Level 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Social Science Level 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Science/Technology Level 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Comparative Study of Cultures</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Analytical Math Level 1</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Writing Intensive</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Level 2 Elective</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Capstone / Writing Intensive II</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Experiential Learning</li>
+            <li class="ui-state-default" onclick="dashboard.shouldGoToCourse($(this));">Cooperative Education</li>
+            <li class="ui-state-highlight" onclick="dashboard.shouldGoToCourse($(this));">General Electives</li>
         </ul>    
     </div>
 </div>
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,29 @@
 var dashboard = {
 
+    //Gets the courseId from the course LI element
+    courseIdFromLI : function(courseLIElement)
+    {
+        var inputElement = courseLIElement.find('input').first();
 
+        if(inputElement != null)
+        {
+            return inputElement.val();
+        }
+        else
+        {
+            return 0;
+        }
+    },
 
+    //Called by clicking on a list element in my_plan
+    //Loads view for that course
+    shouldGoToCourse : function(courseLIElement)
+    {
+        var courseId = myplan.courseIdFromLI(courseLIElement);
 
+        if(courseId !== 0)
+        {
+            window.location.href = BASE_URL + "index.php/course/view/" + courseId;
+        }
+    }
 };
