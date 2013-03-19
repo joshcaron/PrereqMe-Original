5f0bea3a2734cd8036d230103aa3c82e9f5eca7e
Brian Kracoff <bkracoff@gmail.com>
2013-03-18 20:44:27 -0400

    help page

--- application/views/dashboard/help.php
+++ application/views/dashboard/help.php
@@ -1,5 +1,40 @@
 <div id="HELP">
 
-<h1>Coming Soon!</h1>
+<div id="HELP_ACCORDION">
+<h3>What is <em>My Plan</em>?</h3>
+<div><em>My plan</em> is a place for you to plan out your college courses. You can create semesters to place courses that you’ve already completed, courses you’re enrolled in, and courses you plan to take in the future.</div>
+
+<h3>What is <em>Audit</em>?</h3>
+<div>Your <em>audit</em> will help you keep track of all the requirements for your major. It shows you what requirements exist. It also shows you which requirements you’ve already met and which you have left to meet.</div>
+
+<h3>How can I figure out what degree requirements I have completed? </h3>
+<div>If you look at your <em>audit</em> you will be able to see which requirements you have already met.</div>
+
+<h3>How can I find out what degree requirements I have left?  </h3>
+<div>If you look at your <em>audit</em> you will be able to see which requirements you have not yet completed.</div>
+
+<h3>How do I add a course to <em>My Plan</em>? </h3>
+<div>You can add a course to your plan in a couple of different ways. One way is to go to <em>Browse Courses</em>, look for a class, then click the <em>Add to Plan</em> button. Another way is to use the search bar to look up classes, choose a class, then click the <em>Add to Plan</em> button. Lastly, you can go to <em>My Plan</em> and search for classes from there. </div>
+
+<h3>How do I delete a course from <em>My Plan</em>? </h3>
+<div>To delete a course from <em>My Plan</em>, hover over the class. A ‘x’ symbol should show up. Click that ‘x’ and the course will be deleted. </div>
+
+<h3>How do I create a semester? </h3>
+<div>To create a semester go to <em>My Plan</em>. Find the area that says "Add a Semester", choose the semester and year then click "Add". </div>
+
+<h3>How do I delete a semester?  </h3>
+<div>To delete a semester go to <em>My Plan</em>. Once there, hover over the semester you want to delete. An ‘x’ symbol will show up in the upper right hand corner of the semester. Click that ‘x’ to delete the semester. When a pop-up comes up, click "Delete Semester" if you are sure you want to delete it.</div>
+
+</div>
+
+<script>
+$(function() {
+    $( "#HELP_ACCORDION" ).accordion({
+      collapsible: true,
+      heightStyle: "content",
+      active: false
+    });
+  });
+</script>
 
 </div>
