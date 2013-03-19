d8977b11ba21a6bf032b293ed9adf3d1d52a37a2
Brian Kracoff <bkracoff@gmail.com>
2013-03-19 12:11:51 -0400

    fix small search bug

--- assets/js/audit.js
+++ assets/js/audit.js
@@ -21,7 +21,7 @@ var audit = {
     {
         var courseId = audit.courseIdFromLI(courseLIElement);
 
-        if(courseId !== 0)
+        if(courseId !== null)
         {
             window.location.href = BASE_URL + "index.php/course/view/" + courseId;
         }
