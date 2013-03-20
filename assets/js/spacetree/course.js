4edc268759edc130fb9b00a374c137e8f0eff950
Brian Kracoff <bkracoff@gmail.com>
2013-03-20 09:41:26 -0400

    reversing arrow

--- assets/js/spacetree/course.js
+++ assets/js/spacetree/course.js
@@ -35,7 +35,7 @@ $jit.ST.Plot.EdgeTypes.implement({
                 direction = adj.data.$direction,
                 inv = (direction && direction.length > 1 && direction[0] != adj.nodeFrom.id);
 
-            $jit.EdgeHelper.arrow.render(from, to, dim, true, canvas);
+            this.EdgeHelper.arrow.render(from, to, dim, true, canvas);
     }  
   }  
 }); 
